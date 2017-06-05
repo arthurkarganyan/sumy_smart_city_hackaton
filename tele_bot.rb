4b0e00de001c272409b48bc995ff4b04c0ab2d27
arthur <arthur.karganyan@gmail.com>
2017-06-05 14:26:06 +0300

    fx

--- tele_bot.rb
+++ tele_bot.rb
@@ -6,7 +6,7 @@ require 'timeout'
 require_relative "responder.rb"
 require_relative "app/bot_strategy.rb"
 require_relative "app/address_asker.rb"
-require_relative "app/description_asker.rb"
+require_relative "app/desctiption_asker.rb"
 require_relative "app/hello_sayer.rb"
 require_relative "app/location_asker.rb"
 require_relative "app/phone_asker.rb"
